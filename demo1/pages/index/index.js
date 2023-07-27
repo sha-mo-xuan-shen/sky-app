8e9a0b6550fba6e15e5e151c8f9906f2c0fa08fe
chentianran <chentianran@riz-tech.com>
2023-07-27 21:26:34 +0800

    基础完结

--- demo1/pages/index/index.js
+++ demo1/pages/index/index.js
@@ -4,6 +4,7 @@ Page({
     msg:'铸币吧怎么这么菜',
     nickName:'',
     url:'',
+    code:'',
   },
   getUserInfo(){
     wx.getUserProfile({
@@ -14,6 +15,27 @@ Page({
           nickName:res.userInfo.nickName,
           url: res.userInfo.avatarUrl,
         })
+      },
+    })
+  },
+  //微信登录，获取微信用户的授权码
+  wxLogin(){
+    wx.login({
+      success: (res) => {
+        console.log(res.code)
+        this.setData({
+          code: res.code
+        })
+      },
+    })
+  },
+  //发送请求
+  sendRequest(){
+    wx.request({
+      url: 'http://localhost:8080/user/shop/status',
+      method: 'GET',
+      success: (res)=>{
+        console.log(res.data)
       },
     })
   },
