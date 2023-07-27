c872334431cc3f26b8815003893fcd3a578b5107
chentianran <chentianran@riz-tech.com>
2023-07-27 20:51:03 +0800

    获取用户信息

--- demo1/pages/index/index.js
+++ demo1/pages/index/index.js
@@ -1,2 +1,20 @@
 // index.js
-Page({})
+Page({
+  data:{
+    msg:'铸币吧怎么这么菜',
+    nickName:'',
+    url:'',
+  },
+  getUserInfo(){
+    wx.getUserProfile({
+      desc: 'desc',
+      success:(res)=>{
+        console.log(res.userInfo)
+        this.setData({
+          nickName:res.userInfo.nickName,
+          url: res.userInfo.avatarUrl,
+        })
+      },
+    })
+  },
+})
